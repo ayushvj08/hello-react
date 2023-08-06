48b69c56130400eb7ee60fa88bd4b6eb34b5b94a
Ayush Vijayant <ayush.vijayant@gmail.com>
2023-08-07 00:30:06 +0530

    trial 1

--- hello-react/src/TaskCard.jsx
+++ hello-react/src/TaskCard.jsx
@@ -19,3 +19,17 @@ const TaskCard = (props) => {
 };
 
 export default TaskCard;
+
+// import './TaskCard.css'
+// const TaskCard = (props) => {
+//   const date = props.completedAtDate
+//   return (
+//     <div className='TaskItem'>
+//         <h2 className='text-xl font-bold'>{props.title} </h2>
+//       {date == null ? "due date:"+props.dueDate : "completed on:" + props.completedAtDate}
+//         <p>Assignee: {props.assigneeName} </p>
+//       </div>
+//     )
+// }
+
+// export default TaskCard
