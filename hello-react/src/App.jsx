9f55e981bb0c663b42141550e6c774238f091d37
Ayush Vijayant <ayush.vijayant@gmail.com>
2023-08-07 00:38:28 +0530

    trial 2

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -1,43 +1,5 @@
 import TaskCard from "./TaskCard";
 import "./TaskCard.css";
-function App() {
-  return (
-    <>
-      <h1 className="texl-2xl font-bold my-4"> Smarter Task</h1>
-      <div className="grid grid-cols-2 gap-4">
-        <div className="TaskItem">
-          <h1 className="text-xl font-bold my-2">pending</h1>
-          <TaskCard
-            title="Build your website with Static content"
-            dueDate="10th April"
-            assigneeName="Rohit S"
-          />
-          <TaskCard
-            title="Add a blog"
-            dueDate="22nd March"
-            assigneeName="Rohit M"
-          />
-          <h3 className="bg-gray-200 text-black rounded">+ New Task</h3>
-        </div>
-        <div className="TaskItem radius-5 h-full">
-          <h1 className="text-xl font-bold my-2">done</h1>
-          <TaskCard
-            title="Basic html5"
-            completedAtDate="10th April"
-            assigneeName="Rohit M"
-          />
-          <TaskCard
-            title="Nodejs basic"
-            completedAtDate="20th April"
-            assigneeName="Ajay S"
-          />
-        </div>
-      </div>
-    </>
-  );
-}
-export default App;
-
 // function App() {
 //   return (
 //     <>
@@ -74,7 +36,43 @@ export default App;
 //     </>
 //   );
 // }
-// export default App;
+
+function App() {
+  return (
+    <>
+      <h1 className="texl-2xl font-bold my-4"> Smarter Task</h1>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="">
+          <h1 className="text-xl font-bold my-2">pending</h1>
+          <TaskCard
+            title="Build your website with Static content"
+            dueDate="10th April"
+            assigneeName="Rohit S"
+          />
+          <TaskCard
+            title="Add a blog"
+            dueDate="22nd March"
+            assigneeName="Rohit M"
+          />
+          <h3 className="bg-gray-200 text-black rounded">+ New Task</h3>
+        </div>
+        <div className=" radius-5 h-full">
+          <h1 className="text-xl font-bold my-2">done</h1>
+          <TaskCard
+            title="Basic html5"
+            completedAtDate="10th April"
+            assigneeName="Rohit M"
+          />
+          <TaskCard
+            title="Nodejs basic"
+            completedAtDate="20th April"
+            assigneeName="Ajay S"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
 
 // const pps = {
 //   title: "Build website with static content",
@@ -121,4 +119,4 @@ export default App;
 //   );
 // }
 
-// export default App;
+export default App;
--- hello-react/src/TaskCard.jsx
+++ hello-react/src/TaskCard.jsx
@@ -19,17 +19,3 @@ const TaskCard = (props) => {
 };
 
 export default TaskCard;
-
-// import './TaskCard.css'
-// const TaskCard = (props) => {
-//   const date = props.completedAtDate
-//   return (
-//     <div className='TaskItem'>
-//         <h2 className='text-xl font-bold'>{props.title} </h2>
-//       {date == null ? "due date:"+props.dueDate : "completed on:" + props.completedAtDate}
-//         <p>Assignee: {props.assigneeName} </p>
-//       </div>
-//     )
-// }
-
-// export default TaskCard
