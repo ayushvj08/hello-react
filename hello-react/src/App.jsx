ce0d76c795d2e81e789c1c36d02a0a0f0f275d1b
Ayush Vijayant <ayush.vijayant@gmail.com>
2023-08-07 00:06:42 +0530

    lowercase

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -1,43 +1,49 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import TaskCard from "./TaskCard";
-import "./TaskCard.css";
+import "./App.css";
+
+const pps = {
+  title: "Build website with static content",
+  dueDate: "10th April",
+  assigneeName: "Rohit S",
+};
+
 function App() {
   return (
-    <>
-      <h1 className="texl-2xl font-bold my-4"> Smarter Task</h1>
-      <h2></h2>
-      <div className="grid grid-cols-2 gap-4">
-        <div className="TaskItem">
-          <h1 className="text-xl font-bold my-2">pending</h1>
+    <div className="container-div w-4/5 md:w-3/5 mt-12 mx-auto">
+      <h1 className="text-3xl text-gray-600 font-bold my-2">Smarter Tasks</h1>
+      <p className="my-2 text-xl">
+        Project: Graduation Final Year Project (Revamp College Website)
+      </p>
+      <section className="flex flex-row flex-wrap justify-between font-bold mt-4">
+        <div className="border-2 border-gray-500 md:w-5/12 rounded-3xl p-4 pb-32 mb-4">
+          <h1 className="text-center text-gray-500 text-2xl">pending</h1>
+          <TaskCard {...pps} />
           <TaskCard
-            title="Build your website with Static content"
-            dueDate="10th April"
-            assigneeName="Rohit S"
-          />
-          <TaskCard
-            title="Add a blog"
+            title="Add Blog"
             dueDate="22nd March"
             assigneeName="Rohit M"
           />
-          <h3 className="bg-gray-200 text-black rounded">+ New Task</h3>
+
+          <div className="bg-gray-300 p-2 rounded">
+            <p className="font-semibold text-xl text-gray-500 ">+ New Task</p>
+          </div>
         </div>
-        <div className="TaskItem radius-5 h-full">
-          <h1 className="text-xl font-bold my-2">done</h1>
+        <div className="border-2 border-gray-500 md:w-5/12 rounded-3xl p-4">
+          <h1 className="text-center text-gray-500 text-2xl">done</h1>
           <TaskCard
-            title="Basic html5"
+            title="Design the mockup"
             completedAtDate="10th April"
             assigneeName="Rohit M"
           />
           <TaskCard
-            title="Nodejs basic"
+            title="Get Approval from principal"
             completedAtDate="20th April"
             assigneeName="Ajay S"
           />
         </div>
-      </div>
-    </>
+      </section>
+    </div>
   );
 }
+
 export default App;
--- hello-react/src/TaskCard.jsx
+++ hello-react/src/TaskCard.jsx
@@ -1,13 +1,19 @@
 import "./TaskCard.css";
+
 const TaskCard = (props) => {
-  const date = props.completedAtDate;
+  // eslint-disable-next-line react/prop-types
+  const { title, dueDate, completedAtDate, assigneeName } = props;
   return (
-    <div className="TaskItem">
-      <h2 className="text-xl font-bold">{props.title} </h2>
-      {date == null
-        ? "due date:" + props.dueDate
-        : "completed on:" + props.completedAtDate}
-      <p>Assignee: {props.assigneeName} </p>
+    <div className="border-4 border-gray-500 rounded p-4 my-4">
+      <h2 className="text-xl font-bold my-2">{title}</h2>
+      <p className="font-normal">
+        {dueDate
+          ? "Due on: " + dueDate
+          : completedAtDate
+          ? "Completed on: " + completedAtDate
+          : null}
+      </p>
+      <p className="font-normal">Assignee: {assigneeName}</p>
     </div>
   );
 };
