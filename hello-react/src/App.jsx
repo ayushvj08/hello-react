f4f5dfd2d3a3bbf6092f7d6b6ebe9caf29de0598
Ayush Vijayant <ayush.vijayant@gmail.com>
2023-08-07 23:32:48 +0530

    minor

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -10,16 +10,17 @@ function App() {
   return (
     <>
       <div className="bg-white-500 p-4 text-left">
-        <h2 className="text-black text-3xl font-bold mb-2">Smarter Tasks</h2>
+        <h2 className="text-black text-3xl font-bold mb-2">Smarter Task</h2>
         <p className="text-black">
-          Project: Graduation Final Year Project (Revamp College Website)
+          <strong>Project:</strong>
+          <span>Graduation Final Year project (Revamp College Website)</span>
         </p>
       </div>
 
       <div className="flex px-2.5">
         <div className="w-1/2 border border-white-10 rounded-2xl p-4 mr-4 text-left fill-c">
           <h2 className="text-black text-2xl font-bold mb-2 text-center">
-            Pending
+            pending
           </h2>
           <TaskCard
             title="Build the website with static content"
@@ -33,14 +34,15 @@ function App() {
           />
 
           <div className="button">
-            <button className=" hover:bg-white-600 text-black font-bold py-2 px-4 rounded ">
+            <h3 className="font-semibold text-xl text-gray-500 ">+ New Task</h3>
+            {/* <button className=" hover:bg-white-600 text-black font-bold py-2 px-4 rounded ">
               <p>+ New Task</p>
-            </button>
+            </button> */}
           </div>
         </div>
         <div className="w-1/2 border border-white-10 rounded-2xl p-4 ml-4 text-left">
           <h2 className="text-black text-2xl font-bold mb-2 text-center">
-            Done
+            done
           </h2>
           <TaskCard
             title="Design the mockup"
