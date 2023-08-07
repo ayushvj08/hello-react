67b97311f0403a1d79da0fdd1acadc933aec3fda
Ayush Vijayant <ayush.vijayant@gmail.com>
2023-08-08 00:17:37 +0530

    find

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -9,57 +9,53 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl text-gray-600 font-bold my-2">Smarter Task</h1>
-      <p className="my-2 text-xl">
-        Project: Graduation Final Year Project (Revamp College Website)
-      </p>
-      {/* <div className="bg-white-500 p-4 text-left">
+      <div className="bg-white-500 p-4 text-left">
         <h2 className="text-black text-3xl font-bold mb-2">Smarter Tasks</h2>
         <p className="text-black">
           <strong>Project:</strong>{" "}
           <span>Graduation Final Year project (Revamp College Website)</span>
         </p>
-      </div> */}
 
-      <div className="flex px-2.5">
-        <div className="w-1/2 border border-white-10 rounded-2xl p-4 mr-4 text-left fill-c">
-          <h2 className="text-black text-2xl font-bold mb-2 text-center">
-            Pending
-          </h2>
-          <TaskCard
-            title="Build the website with static content"
-            dueDate="10th April"
-            assigneeName="Rohit S"
-          />
-          <TaskCard
-            title="Add a Blog"
-            dueDate="22nd March"
-            assigneeName="Rohit M"
-          />
+        <div className="flex px-2.5">
+          <div className="w-1/2 border border-white-10 rounded-2xl p-4 mr-4 text-left fill-c">
+            <h2 className="text-black text-2xl font-bold mb-2 text-center">
+              Pending
+            </h2>
+            <TaskCard
+              title="Build the website with static content"
+              dueDate="10th April"
+              assigneeName="Rohit S"
+            />
+            <TaskCard
+              title="Add a Blog"
+              dueDate="22nd March"
+              assigneeName="Rohit M"
+            />
 
-          <div className="button">
-            <button
-              type="button"
-              className=" hover:bg-white-600 text-black font-bold py-2 px-4 rounded "
-            >
-              <p>+ New Task</p>
-            </button>
+            <div className="button">
+              <button
+                type="button"
+                className=" hover:bg-white-600 text-black font-bold py-2 px-4 rounded "
+              >
+                <p>+ New Task</p>
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="w-1/2 border border-white-10 rounded-2xl p-4 ml-4 text-left">
-          <h2 className="text-black text-2xl font-bold mb-2 text-center">
-            Done
-          </h2>
-          <TaskCard
-            title="Design the mockup"
-            completedAtDate="10th April"
-            assigneeName="Rohit M"
-          />
-          <TaskCard
-            title="Get the approval from principal"
-            completedAtDate="20th April"
-            assigneeName="Ajay S"
-          />
+          <div className="w-1/2 border border-white-10 rounded-2xl p-4 ml-4 text-left">
+            <h2 className="text-black text-2xl font-bold mb-2 text-center">
+              Done
+            </h2>
+            <TaskCard
+              title="Design the mockup"
+              completedAtDate="10th April"
+              assigneeName="Rohit M"
+            />
+            <TaskCard
+              title="Get the approval from principal"
+              completedAtDate="20th April"
+              assigneeName="Ajay S"
+            />
+          </div>
         </div>
       </div>
     </>
