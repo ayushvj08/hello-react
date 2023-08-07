1e5eb2591ef8bf85942ae3b1ca4b546cd102e750
Ayush Vijayant <ayush.vijayant@gmail.com>
2023-08-07 23:25:01 +0530

    remove child tags of p

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -1,48 +1,58 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 import TaskCard from "./TaskCard";
-import "./TaskCard.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      <div className="container-div w-4/5 md:w-3/5 mt-12 mx-auto">
-        <h1 className="text-3xl text-gray-600 font-bold my-2">Smarter Task</h1>
-        <p className="my-2 text-xl">
+      <div className="bg-white-500 p-4 text-left">
+        <h2 className="text-black text-3xl font-bold mb-2">Smarter Tasks</h2>
+        <p className="text-black">
           Project: Graduation Final Year Project (Revamp College Website)
         </p>
-        <section className="flex flex-row flex-wrap justify-between font-bold mt-4">
-          <div className="border-2 border-gray-500 md:w-5/12 rounded-3xl p-4 pb-32 mb-4">
-            <h1 className="text-center text-gray-500 text-2xl">pending</h1>
-            <TaskCard
-              title="Build website with static content"
-              dueDate="10th April"
-              assigneeName="Rohit S"
-            />
-            <TaskCard
-              title="Add Blog"
-              dueDate="22nd March"
-              assigneeName="Rohit M"
-            />
+      </div>
 
-            <div className="bg-gray-300 p-2 rounded">
-              <h3 className="font-semibold text-xl text-gray-500 ">
-                + New Task
-              </h3>
-            </div>
+      <div className="flex px-2.5">
+        <div className="w-1/2 border border-white-10 rounded-2xl p-4 mr-4 text-left fill-c">
+          <h2 className="text-black text-2xl font-bold mb-2 text-center">
+            Pending
+          </h2>
+          <TaskCard
+            title="Build the website with static content"
+            dueDate="10th April"
+            assigneeName="Rohit S"
+          />
+          <TaskCard
+            title="Add a Blog"
+            dueDate="22nd March"
+            assigneeName="Rohit M"
+          />
+
+          <div className="button">
+            <button className=" hover:bg-white-600 text-black font-bold py-2 px-4 rounded ">
+              <p>+ New Task</p>
+            </button>
           </div>
-          <div className="border-2 border-gray-500 md:w-5/12 rounded-3xl p-4">
-            <h1 className="text-center text-gray-500 text-2xl">done</h1>
-            <TaskCard
-              title="Design the mockup"
-              completedAtDate="10th April"
-              assigneeName="Rohit M"
-            />
-            <TaskCard
-              title="Get Approval from principal"
-              completedAtDate="20th April"
-              assigneeName="Ajay S"
-            />
-          </div>
-        </section>
+        </div>
+        <div className="w-1/2 border border-white-10 rounded-2xl p-4 ml-4 text-left">
+          <h2 className="text-black text-2xl font-bold mb-2 text-center">
+            Done
+          </h2>
+          <TaskCard
+            title="Design the mockup"
+            completedAtDate="10th April"
+            assigneeName="Rohit M"
+          />
+          <TaskCard
+            title="Get the approval from principal"
+            completedAtDate="20th April"
+            assigneeName="Ajay S"
+          />
+        </div>
       </div>
     </>
   );
