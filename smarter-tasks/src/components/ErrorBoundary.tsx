61175fbb92be51d0be892241fb780cb9f2cb2d88
Ayush Vijayant <ayush.vijayant@gmail.com>
2023-09-02 19:50:14 +0530

    comment ErrorBoundary: eslint raising build error

--- smarter-tasks/src/components/ErrorBoundary.tsx
+++ smarter-tasks/src/components/ErrorBoundary.tsx
@@ -1,33 +1,33 @@
-/* eslint-disable @typescript-eslint/ban-types */
-import { Component, ErrorInfo, ReactNode } from "react";
+// /* eslint-disable @typescript-eslint/ban-types */
+// import { Component, ErrorInfo, ReactNode } from "react";
 
-interface ErrorBoundaryState {
-  hasError: boolean;
-}
+// interface ErrorBoundaryState {
+//   hasError: boolean;
+// }
 
-class ErrorBoundary extends Component<{}, ErrorBoundaryState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = { hasError: false };
-  }
+// class ErrorBoundary extends Component<{}, ErrorBoundaryState> {
+//   constructor(props: {}) {
+//     super(props);
+//     this.state = { hasError: false };
+//   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
-    return { hasError: true };
-  }
+//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//   static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+//     return { hasError: true };
+//   }
 
-  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
-    // You can log the error or send it to an error reporting service
-    console.error("ErrorBoundary caught an error:", error, errorInfo);
-  }
+//   componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+//     // You can log the error or send it to an error reporting service
+//     console.error("ErrorBoundary caught an error:", error, errorInfo);
+//   }
 
-  render(): ReactNode {
-    if (this.state.hasError) {
-      return <div>Something went wrong.</div>;
-    }
+//   render(): ReactNode {
+//     if (this.state.hasError) {
+//       return <div>Something went wrong.</div>;
+//     }
 
-    return this.props.children;
-  }
-}
+//     return this.props.children;
+//   }
+// }
 
-export default ErrorBoundary;
+// export default ErrorBoundary;
